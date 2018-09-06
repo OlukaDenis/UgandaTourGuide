aee49c149478014469428e42fc23149602ce4437
Oluka Denis <olukadeno@gmail.com>
2018-09-06 10:23:34 +0300

    Added descriptions to the string.xml

--- app/src/main/java/com/mcdenny/ugandatourguide/ui/HotelFragment.java
+++ app/src/main/java/com/mcdenny/ugandatourguide/ui/HotelFragment.java
@@ -39,15 +39,18 @@ public class HotelFragment extends android.support.v4.app.Fragment {
        View rootView = inflater.inflate(R.layout.grid_view_list, container, false);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word( "Hotel Africana","Hotel Africana is Uganda\'s Premier luxury Hotel. The hotel has over 14 fully furnished well air-conditioned.On a leafy property surrounded by shops and restaurants, this elegant hotel",
-                "40, Airtel House, Wampewo Ave, Kampala","041 4777500", "http://www.hotelafricana.com", R.drawable.hotelafricana));
-        words.add(new Word( "Sheraton Hotel","The upscale rooms and suites come with free Wi-Fi, satellite TV and safes. They also include tea and coffeemakers. Room service is available 24/7. Dining options include a gourmet restaurant, an Italian bistro and an eatery with an international menu.",
-                "Kintu Road, Kampala","031 2309000", "https://www.serenahotels.com/serenakampala", R.drawable.sheraton));
-        words.add(new Word( "Speke Hotel","Warmly decorated, low-key rooms offer free Wi-Fi, flat-screen TVs, and minibars, as well as tea and coffeemaking facilities. Upgraded rooms add living areas, and some have 4-poster beds.\n" +
-                "Breakfast and parking are free, and a relaxed restaurant offers Indian fare.",
-                "Nile Avenue Kimathi Ave, Kampala", "041 2592214", "https://www.spekehotel.com", R.drawable.spekehotel));
-        words.add(new Word( "Grand Imperial Hotel","Hotel Africana is Uganda\'s Premier luxury Hotel. The hotel has over 14 fully furnished well air-conditioned",
-                "Plot 2A/B/P.O.Box 7048 Colville St, Kampala","041 4311048", "https://www.imperialhotels.co.ug", R.drawable.grandimperial));
+        words.add(new Word( "Hotel Africana",getString(R.string.africana_desc),
+                "40, Airtel House, Wampewo Ave, Kampala","041 4777500",
+                "http://www.hotelafricana.com", R.drawable.hotelafricana));
+        words.add(new Word( "Sheraton Hotel",getString(R.string.sheraton_desc),
+                "Kintu Road, Kampala","031 2309000",
+                "https://www.serenahotels.com/serenakampala", R.drawable.sheraton));
+        words.add(new Word( "Speke Hotel",getString(R.string.speke_desc),
+                "Nile Avenue Kimathi Ave, Kampala", "041 2592214",
+                "https://www.spekehotel.com", R.drawable.spekehotel));
+        words.add(new Word( "Grand Imperial Hotel",getString(R.string.grand_desc),
+                "Plot 2A/B/P.O.Box 7048 Colville St, Kampala","041 4311048",
+                "https://www.imperialhotels.co.ug", R.drawable.grandimperial));
 
         WordAdapter wordAdapter = new WordAdapter(getActivity(), words);
         GridView gridView = (GridView) rootView.findViewById(R.id.list);
--- app/src/main/java/com/mcdenny/ugandatourguide/ui/RestaurantFragment.java
+++ app/src/main/java/com/mcdenny/ugandatourguide/ui/RestaurantFragment.java
@@ -37,14 +37,18 @@ public class RestaurantFragment extends android.support.v4.app.Fragment {
        View rootView = inflater.inflate(R.layout.grid_view_list, container, false);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word( "Cafe Javas","Hotel Africana is Uganda\'s Premier luxury Hotel. The hotel has over 14 fully furnished well air-conditioned",
-                "Kampala Boulevard, Kampala Road","039 2177284", "https://cafejavas.co.ug", R.drawable.cafejavas));
-        words.add(new Word( "Biryani House","There is a lot of India restaurants in kampala, but we are the best in terms of Biryani. Enjoy the best food at Biryani house which specifically located in Kampala.",
-                "John Babiha (Acacia) Ave, Kampala", " 0758 935839", "www.africana.com", R.drawable.biryani_house));
-        words.add(new Word( "Silver City Spur","They have great cocktails and snacks.  The ambience is also good and the waiters and waitresses are very polite.",
-                "Garden City Shopping Centre, Yusuf Lule Rd, Kampala","041 4563500", "https://www.spurinternational.com/Uganda", R.drawable.city_spur));
-        words.add(new Word( "Fang Fang Chinese Restaurant","We have all chinese dishes that you have ever heard of. Our magnificent restaurant",
-                "Plot 1, Conville Street, Kampala 6323","0773047940", "http://fangfang.co.ug", R.drawable.fangfang));
+        words.add(new Word( "Cafe Javas",getString(R.string.cafe_desc),
+                "Kampala Boulevard, Kampala Road","039 2177284",
+                "https://cafejavas.co.ug", R.drawable.cafejavas));
+        words.add(new Word( "Biryani House",getString(R.string.biryani_desc),
+                "John Babiha (Acacia) Ave, Kampala", " 0758 935839",
+                "www.africana.com", R.drawable.biryani_house));
+        words.add(new Word( "Silver City Spur",getString(R.string.spur_desc),
+                "Garden City Shopping Centre, Yusuf Lule Rd, Kampala", "041 4563500",
+                "https://www.spurinternational.com/Uganda", R.drawable.city_spur));
+        words.add(new Word( "Fang Fang Chinese Restaurant",getString(R.string.fang_desc),
+                "Plot 1, Conville Street, Kampala 6323","0773047940",
+                "http://fangfang.co.ug", R.drawable.fangfang));
 
         WordAdapter wordAdapter = new WordAdapter(getActivity(), words);
         GridView gridView = (GridView) rootView.findViewById(R.id.list);
